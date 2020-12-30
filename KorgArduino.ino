63f6c82ba1132fa427b9c34a573b67fffbe39469
brendena <cerebralasylum1@gmail.com>
2020-12-30 08:06:41 -0500

    it works

--- KorgArduino.ino
+++ KorgArduino.ino
@@ -2,11 +2,22 @@
 
 AltSoftSerial altSerial; //9 tx 8 rx 
 
-#define MIDI_CONTROL       0xB0
-#define MIDI_NOTE_ON       0x80
-#define MIDI_NOTE_OFF      0x90
-#define MIDI_CHANNEL       0x01
+// midi protocol status
+#define MIDI_CONTROL           0xB0
+#define MIDI_NOTE_ON           0x90
+#define MIDI_NOTE_OFF          0x80
 
+
+
+
+// midi protocol data
+#define MIDI_MODULATION_WHEEl  0x01
+
+
+//korg midi protocol status
+#define KORG_MIDI_CHANNEL           0x01 //so this is ch 2 on the korg
+
+// korg midi protocol data
 #define KORG_TOUCH_PAD_X_AXIS   12
 #define KORG_TOUCH_PAD_Y_AXIS   13
 #define KORG_TOUCH_PAD_ON_OFF   92
@@ -14,10 +25,11 @@ AltSoftSerial altSerial; //9 tx 8 rx
 
 void setup()
 {
-   // Open serial communications and wait for port to open:
+   //standard serial port 
   Serial.begin(115200);
   while (!Serial) {}
-  
+
+  //alternative serial port for midi 
   altSerial.begin(31250);    
 }
 
@@ -33,63 +45,61 @@ void MIDI_TX(byte MESSAGE, byte PITCH, byte VELOCITY,byte midichannel)
 }
 
 
-unsigned char buffer[3];
-unsigned char bufferI = 0;
+unsigned char midiBuffer[3];
+unsigned char midiBufferI = 0;
+unsigned char volume = 0x7F;
 
 void loop() // run over and over
 {
   //*
   if (altSerial.available()){
     unsigned char readValue = altSerial.read();
-    Serial.print("got some data");
-    /*
-    if((readValue & 0b10000000) == true)
+    Serial.print(readValue,HEX);
+    Serial.print(",");
+
+    //if it's a status byte reset the midi buffer
+    if((readValue & 0b10000000) != 0)
     {
-      bufferI = 0;
+      midiBufferI = 0;
     }
-    */
-    buffer[bufferI] = readValue;
-    bufferI++;
-    if(bufferI == 3)
+    midiBuffer[midiBufferI] = readValue;
+    midiBufferI++;
+    
+    //when you've got a full buffer
+    if(midiBufferI == 3)
     {
-      bufferI = 0;
-      /*
-      if(readValue & MIDI_NOTE_ON  == true ||
-         readValue & MIDI_NOTE_OFF == true )
+      midiBufferI = 0;
+      //*
+      if((midiBuffer[0] & 0xf0)  == MIDI_NOTE_ON  ||
+         (midiBuffer[0] & 0xf0) == MIDI_NOTE_OFF)
       {
-        MIDI_TX(MIDI_CONTROL,KORG_TOUCH_PAD_X_AXIS,buffer[1],MIDI_CHANNEL);
-        MIDI_TX(MIDI_CONTROL,KORG_TOUCH_PAD_Y_AXIS,0,MIDI_CHANNEL);
-        if(readValue & MIDI_NOTE_ON == true){
-          MIDI_TX(MIDI_CONTROL,KORG_TOUCH_PAD_ON_OFF,127,MIDI_CHANNEL);
+        MIDI_TX(MIDI_CONTROL,KORG_TOUCH_PAD_X_AXIS,midiBuffer[1],KORG_MIDI_CHANNEL);
+        MIDI_TX(MIDI_CONTROL,KORG_TOUCH_PAD_Y_AXIS,volume,KORG_MIDI_CHANNEL);
+        if((midiBuffer[0] & 0xf0)  == MIDI_NOTE_ON ){
+          MIDI_TX(MIDI_CONTROL,KORG_TOUCH_PAD_ON_OFF,127,KORG_MIDI_CHANNEL);
         }
-        else if(readValue & MIDI_NOTE_ON == true){
-          MIDI_TX(MIDI_CONTROL,KORG_TOUCH_PAD_ON_OFF,0,MIDI_CHANNEL);
+        else if((midiBuffer[0] & 0xf0) == MIDI_NOTE_OFF){
+          MIDI_TX(MIDI_CONTROL,KORG_TOUCH_PAD_ON_OFF,0,KORG_MIDI_CHANNEL);
         }
       }
-      */
-      Serial.print("got something - ");
+      else if((midiBuffer[0] & 0xf0)  == MIDI_CONTROL)
+      {
+        if(midiBuffer[1] == MIDI_MODULATION_WHEEl)
+        {
+          volume = midiBuffer[2];
+          MIDI_TX(MIDI_CONTROL,KORG_TOUCH_PAD_Y_AXIS,volume,KORG_MIDI_CHANNEL);
+        }
+      }
+      //*
+      Serial.print("RX - ");
       for(int i =0; i < 3; i++)
       {
-        Serial.print(buffer[i],HEX);
+        Serial.print(midiBuffer[i],HEX);
         Serial.print(",");
       }
       Serial.print("\n");
-    }
-  }
-  //*/
-  /*
-  while(true){
-    int i =1;
-    for(int j =0; j < 127; j++)
-    {
-      MIDI_TX(0xB0,12,j,i);
-      MIDI_TX(0xB0,13,j,i);
-      MIDI_TX(0xB0,92,127,i);
-      delay(100);
-      MIDI_TX(0xB0,92,0,i);
-      delay(100);
+      //*
     }
   }
   //*/
 }
-
